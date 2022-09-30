55504ba100d3c21cddfe9a653a83b32a3db725ad
chrisYang256 <maxsummer256@gmail.com>
2022-09-30 16:55:19 +0900

    spring bean and dependency

---
+++ 01_hello_spring/src/main/java/my/frist/hellospring/service/BeanConfig.java
@@ -0,0 +1,21 @@
+// package my.frist.hellospring.service;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// import my.frist.hellospring.repository.MemberRepository;
+// import my.frist.hellospring.repository.MemoryMemberRepository;
+
+// @Configuration
+// public class BeanConfig {
+    
+//     @Bean
+//     public MemberService memberService() {
+//         return new MemberService(memberRepository());
+//     }
+
+//     @Bean
+//     public MemberRepository memberRepository() {
+//         return new MemoryMemberRepository();
+//     }
+// }
