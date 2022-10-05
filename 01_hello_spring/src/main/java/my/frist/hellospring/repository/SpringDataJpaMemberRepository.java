9bb820bce521785724a7034669574db711a1cbc7
chrisYang256 <maxsummer256@gmail.com>
2022-10-05 16:37:12 +0900

    add: spring data jpa

---
+++ 01_hello_spring/src/main/java/my/frist/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,18 @@
+package my.frist.hellospring.repository;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import my.frist.hellospring.domain.Member;
+
+// interface가 interface를 받을 때는 extends, interface는 다중 상속 가능
+// interface SpringDataJpaMemberRepository extends JpaRepository를 통해 spring data jpa가 자동으로 구현체를 만들고 spring bean에 등록해줌
+// 이후 config에서 injection 등을 하고 service에 DI해주면 됨
+@Primary // 해당 타입의 bean이 여러개인 경우 @Primary 등으로 우선순위를 주어야함.
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    
+    @Override
+    Optional<Member> findByName(String name);
+}
--- 01_hello_spring/src/main/java/my/frist/hellospring/service/SpringConfig.java
+++ 01_hello_spring/src/main/java/my/frist/hellospring/service/SpringConfig.java
@@ -2,6 +2,7 @@ package my.frist.hellospring.service;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,16 +13,25 @@ import my.frist.hellospring.repository.MemberRepository;
 public class SpringConfig {
 
     // private DataSource dataSource;
-
+    // private EntityManager em;
+    
     // @Autowired
-    // public SpringConfig(DataSource dataSource) {
+    // public SpringConfig(DataSource dataSource, EntityManager em) {
     //     this.dataSource = dataSource;
+    //     this.em = em;
     // }
 
-    private EntityManager em;
-   
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    // interface SpringDataJpaMemberRepository extends JpaRepository를 통해 spring bean에 등록을 해놓기 때문에 따로 등록할 필요 없음
+    private final MemberRepository memberRepository;
+    
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
     // @Bean
@@ -29,11 +39,11 @@ public class SpringConfig {
     //     return new MemberService(memberRepository());
     // }
 
-    @Bean
-    public MemberRepository memberRepository() {
+    // @Bean
+    // public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
         // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        // return new JpaMemberRepository(em);
+    // }
 }
