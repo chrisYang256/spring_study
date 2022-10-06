2f77188b5c5c0ea0ca58020062093790ddce6be9
chrisYang256 <maxsummer256@gmail.com>
2022-10-06 15:05:48 +0900

    add/ mod: comments

--- 01_hello_spring/src/main/java/my/first/hellospring/controller/MemberController.java
+++ 01_hello_spring/src/main/java/my/first/hellospring/controller/MemberController.java
@@ -16,6 +16,8 @@ import my.first.hellospring.service.MemberService;
 public class MemberController {
     
     // MemberService를 new로 새로운 객체를 생성하는 경우  
+    // 다른 곳들에서 MemberService를 사용하는데, 여러개의 인스턴스를 생성할 필요 없기 때음
+    // 때문에 spring bean에 service 등록/ @Service, @Repository, @Autowired를 통해 스프링 컨테이너에 등록하여 공유 사용
     // private final MemberService memberService = new MemberService();
 
     private final MemberService memberService;
@@ -24,7 +26,7 @@ public class MemberController {
     // !! MemberService에 @Service, Repository에는 @Repository라는 annotation이 있어야 스프링 컨테이너에서 인식할 수 있음
     // MemberController가 생성될 때 spring bean에 등록된 @Service 등을 넣어줌 -> 의존관계 주입(DI, Defendency Injection)
     // @service 등의 annotation을 통해 spring bean에 등록하는 방식을 component scan이라고 함
-    // configuration 파일을 만들고 @Bean 등을 통해 코드로 직접 sporing bean에 등록하는 방식도 있음(여기서는 service/BeanConfig.java에 코딩, 이 방식의 경우 Controller를 제외한 service 등에서 @Service, @Autowired 등을 지워야함)(controller는 어차피 스프링이 관리하기 때문에 component scan으로 올라가고 따라서 controller의 @Autowired를 안지워도 됨)
+    // configuration 파일을 만들고 @Bean 등을 통해 코드로 직접 sporing bean에 등록하는 방식도 있음(여기서는 service/BeanConfig.java에 코딩, 이 방식의 경우 Controller를 제외한 service 등에서 @Service, @Autowired 등을 지워야함)(controller는 어차피 스프링이 관리하기 때문에 component scan을 통해 올라가므로 controller의 @Autowired를 안지워도 됨)
     // 필드 주입 방식(@Autowired private MemberService memberService)은 아래와 같은 생성자 필요없으나, 중간에 바꿀 수 있는 방법이 없음. 
     // setter 주입 방식은 어디서든 호출할 수 있기 때문에 추천되지 않음
     // 생성자 주입 방식은 스프링 컨테이너가 올라가며 세팅되는 시점에 들어가고 끝나기 때문에 의존관계가 실행 중 동적으로 변하는 경우가 없어 권장
